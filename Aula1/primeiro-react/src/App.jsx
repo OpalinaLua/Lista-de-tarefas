7226fbaa26a6db0ad74dec1676c286ad4bc8051a
OpalinaLua <Luamdarabello@gmail.com>
2025-06-27 18:41:38 -0300

    blah

--- Aula1/primeiro-react/src/App.jsx
+++ Aula1/primeiro-react/src/App.jsx
@@ -1,37 +1,12 @@
 import { useState } from 'react'
+import './global.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          contagem é {count}
-        </button>
-        <p>
-          Edite <code>src/App.jsx</code> e salve para testar o HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-         Clique nos logotipos Vite e React para saber mais
-      </p>
-      <footer>
-        <b className='rodape'>
-          Firjan Senai - Front End - 2025
-        </b>
-      </footer>
-    </>
+  
+  return(
+    <></>
   )
+  
 }
 
 export default App
