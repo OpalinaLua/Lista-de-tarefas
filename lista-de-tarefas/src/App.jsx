1c31fdf6232cf72baf1ae39310a4bd14abcb72df
OpalinaLua <Luamdarabello@gmail.com>
2025-07-01 21:32:00 -0300

    sd

--- lista-de-tarefas/src/App.jsx
+++ lista-de-tarefas/src/App.jsx
@@ -5,12 +5,29 @@ import { Footer } from "./Components/Footer/Footer";
 import { Header } from "./Components/Header/Header";
 import { Contador } from "./Components/Contador/Contador";
 import { ContadorStateless } from "./Components/ContadorStateless.jsx/ContadorStateless";
+import { useState } from "react";
 
 function App() {
+  const [wishs, setWishs] = useState([]);
+  const [form, setsform] = useState({
+    name: "",
+    descripition: "",
+  });
+  const handleAChange = (newItem) => {
+    const { name, value } = e.target;
+    setsform((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+    const newWishs = { name: newItem.name, descripition: newItem.descripition };
+    const update = [...wishs, newWishs];
+    setWishs(updateWishs);
+  };
   return (
     <div className={styles.app}>
       <Header />
       <main className={styles.main}>
+        <form action=""></form>
         <CardGrid />
       </main>
       <Footer />
--- lista-de-tarefas/src/Components/Card/Card.jsx
+++ lista-de-tarefas/src/Components/Card/Card.jsx
@@ -1,13 +1,6 @@
 import styles from "./Card.module.css";
-import { Contador } from "../Contador/Contador";
-import { ContadorStateless } from "../ContadorStateless.jsx/ContadorStateless";
-import { useState } from "react";
 
 export const Card = ({ name, description, image }) => {
-  const [contador, setContador] = useState(1);
-  const [nome, setNome] = useState("");
-  const [nome2, setNome2] = useState("");
-
   const adicionarContador = () => {
     setContador((prevState) => prevState + 1);
   };
@@ -16,41 +9,10 @@ export const Card = ({ name, description, image }) => {
     console.log("Nome: ", nome2);
   };
   return (
-    <>
-      <div className={styles.card}>
-        <div className={styles.imgContainer}>
-          <img src={image} alt={name} className={styles.image} />
-        </div>
-        <div className={styles.content}>
-          <h3 className={styles.name}>{name}</h3>
-          <p className={styles.description}>{description}</p>
-
-          <Contador />
-          <ContadorStateless
-            contador={contador}
-            adicionarContador={adicionarContador}
-          />
-          <ContadorStateless
-            contador={contador}
-            adicionarContador={adicionarContador}
-          />
-          <h1>{nome}</h1>
-          <input
-            type="text"
-            value={nome}
-            onChange={(e) => setNome(e.target.value)}
-          />
-          <h1>{nome2}</h1>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              value={nome2}
-              onChange={(e) => setNome2(e.target.value)}
-            />
-            <button type="submit">atualizar estado</button>
-          </form>
-        </div>
+    <div className={styles.card}>
+      <div className={styles.imgContainer}>
+        <img src={image} alt={name} className={styles.image} />
       </div>
-    </>
+    </div>
   );
 };
