5a7a7c59bb595f772f19c20ed79a254a187a972f
OpalinaLua <Luamdarabello@gmail.com>
2025-07-04 21:14:03 -0300

    cont

--- lista-de-tarefas/src/Components/Empty/Empty.jsx
+++ lista-de-tarefas/src/Components/Empty/Empty.jsx
@@ -3,18 +3,20 @@ import styles from "./Empty.module.css";
 export const Empty = ({ isSearching }) => {
   return (
     <>
-      {isSearching ? (
-        <div className={styles.container}>
-          <p className={styles.icon}>🙁✨</p>
-          <p className={styles.tex}>Nenhum desejo brilhando por aqui</p>
-        </div>
-      ) : (
-        <div className={styles.container}>
-          <p className={styles.tex}>Sem desejos cadastrados...</p>
-          <p className={styles.icon}>👀 </p>
-          <p className={styles.tex}>nada por aqui!</p>
-        </div>
-      )}
+      <div className={styles.container}>
+        {isSearching ? (
+          <div className={styles.MSGcontainer}>
+            <p className={styles.icon}>🙁✨</p>
+            <p className={styles.tex}>Nenhum desejo brilhando por aqui</p>
+          </div>
+        ) : (
+          <div className={styles.container}>
+            <p className={styles.tex1}>Sem desejos cadastrados...</p>
+            <p className={styles.icon}>👀 </p>
+            <p className={styles.tex1}>nada por aqui!</p>
+          </div>
+        )}
+      </div>
     </>
   );
 };
