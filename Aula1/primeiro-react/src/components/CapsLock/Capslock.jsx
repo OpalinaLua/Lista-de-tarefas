7b748e040ff272926911f78caa015269eef0ee66
OpalinaLua <Luamdarabello@gmail.com>
2025-07-04 21:40:29 -0300

    gsf

--- Aula1/primeiro-react/src/components/CapsLock/Capslock.jsx
+++
@@ -1,8 +0,0 @@
-function CapsLock(props){
-    const textoInserido= props.children
-    const textoCaps= textoInserido.toUpperCase()
-
-    return <div>{textoCaps}</div>
-   
-}
-export default CapsLock
